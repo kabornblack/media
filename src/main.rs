d836b61fd8534bcc09cb4a77801d30495e5b3ea2
kabornblack <shaibu.kabiru@email.com>
2025-07-15 17:50:21 +0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+enum Media {
+    Book { title: String, author: String },
+    Movie { title: String, director: String },
+    Audiobook { title: String },
+}
+
+fn print_media(media: Media) {
+    println!("{:#?}", media);
+}
+
+fn main() {
+    let audiobook = Media::Audiobook {
+        title: String::from("An Audiobook"),
+    };
+    let good_movie = Media::Movie {
+        title: String::from("Good Movie"),
+        director: String::from("Good Director"),
+    };
+    let bad_book = Media::Book {
+        title: String::from("Bad Book"),
+        author: String::from("Bad Author"),
+    };
+
+    print_media(good_movie);
+    print_media(bad_book);
+    print_media(audiobook);
+}
