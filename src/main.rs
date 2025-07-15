83db88c8e55222e20a4be307a4869ea4d11a7614
kabornblack <shaibu.kabiru@email.com>
2025-07-15 22:42:53 +0300

    Add media catalog system with support for multiple media types and safe indexing

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,65 @@ enum Media {
     Book { title: String, author: String },
     Movie { title: String, director: String },
     Audiobook { title: String },
+    Podcast(u32),
+    Placeholder,
+}
+
+impl Media {
+    fn description(&self) -> String {
+        // if let Media::Book { title, author } = self {
+        //     format!("Book: {} {}", title, author)
+        // } else if let Media::Movie { title, director } = self {
+        //     format!("Movie: {} {}", title, director)
+        // } else if let Media::Audiobook { title } = self {
+        //     format!("Audiobook: {}", title)
+        // } else {
+        //     String::from("Media description")
+        // }
+
+        match self {
+            Media::Book { title, author } => {
+                format!("Book: {} {}", title, author)
+            }
+            Media::Movie { title, director } => {
+                format!("Movie: {} {}", title, director)
+            }
+            Media::Audiobook { title } => {
+                format!("Audiobook: {}", title)
+            }
+            Media::Podcast(id) => {
+                format!("Podcast: {}", id)
+            }
+            Media::Placeholder => {
+                format!("Placeholder")
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Catalog {
+    items: Vec<Media>,
+}
+
+impl Catalog {
+    fn new() -> Self {
+        Catalog { items: vec![] }
+    }
+
+    fn add(&mut self, media: Media) {
+        self.items.push(media);
+    }
+
+    fn get_by_index(&self, index: usize) -> Option<&Media> {
+        if self.items.len() > index {
+            // Good! We have somethign to return
+            Some(&self.items[index])
+        } else {
+            // Bad! We don't have anything to return!!!
+            None
+        }
+    }
 }
 
 fn print_media(media: Media) {
@@ -21,8 +80,37 @@ fn main() {
         title: String::from("Bad Book"),
         author: String::from("Bad Author"),
     };
+    let podcast = Media::Podcast(10);
+    let placeholder = Media::Placeholder;
 
-    print_media(good_movie);
-    print_media(bad_book);
-    print_media(audiobook);
+    // println!("{}", audiobook.description());
+    // println!("{}", good_movie.description());
+    // println!("{}", bad_book.description());
+
+    let mut catalog = Catalog::new();
+
+    catalog.add(audiobook);
+    catalog.add(good_movie);
+    catalog.add(bad_book);
+    catalog.add(podcast);
+    catalog.add(placeholder);
+
+    // let item = catalog.get_by_index(40);
+
+    // println!("{:#?}", item);
+
+    match catalog.get_by_index(1) {
+        Some(value) => {
+            println!("Item: {:#?}", value);
+        }
+        None => {
+            println!("No value here!");
+        }
+    }
+
+    // if let Some(value) = catalog.get_by_index(0) {
+    //     println!("Item in pattern match: {:#?}", value)
+    // } else {
+    //     println!("No value!!!!!!!!!!");
+    // }
 }
